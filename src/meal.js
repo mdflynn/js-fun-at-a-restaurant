929d8a67f356c12eaf9cb11bb030efe0a709ea77
mdflynn <mdflynn34@outlook.com>
2020-07-03 10:19:17 -0600

    Add meal and order

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,37 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return (`Delicious ${name}`)
+};
 
-}
+function createMenuItem(name, price, type) {
+  return {
+    name,
+    price,
+    type}
+};
 
+function addIngredients(newIngred, ingredients) {
+  if (!ingredients.includes(newIngred)) {
+    ingredients.push(newIngred);
+  }
+};
+
+function formatPrice(price) {
+  return `$${price}`
+};
+
+function decreasePrice(price) {
+  return price * .9;
+};
+
+function createRecipe(title, ingredients, type) {
+  return {title, ingredients, type};
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(order, delivery) {
+  if (delivery.length < 3) {
+  delivery.push(order)
+  }
+};
+
+function refundOrder(order, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === order) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+};
+
+function listItems(deliveryOrders) {
+  var items = '';
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (i === deliveryOrders.length - 1) {
+      items += deliveryOrders[i].item;
+      break;
+    };
+    items += deliveryOrders[i].item;
+    items += ", ";
+  }
+  return items;
+};
+
+function searchOrder(deliveryOrders, item) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === item) {
+      return true;
+    } else {
+      return false;
+    }
+}
+};
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
