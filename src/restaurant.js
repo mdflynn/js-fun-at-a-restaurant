e89e7f17cc05e5ce945cf09aa642459470886727
mdflynn <mdflynn34@outlook.com>
2020-07-05 15:37:39 -0600

    Add restaurant.js update

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,52 @@
+function createRestaurant(name) {
+  return {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+};
+
+function addMenuItem(shopName, item) {
+  if (!shopName.menus[item.type].includes(item)) {
+    shopName.menus[item.type].push(item);
+  }
+};
+
+
+
+// function removeMenuItem(restaurant, item) {
+//   var message = `No one is eating our ${item.name}
+//     - it has been removed from the breakfast menu!`;
+//
+//     if (restaurant.menus[item.type].inclues(item)) {
+//       this.restaurant.menus[item.type].pop(item);
+//       return message;
+//     }
+//
+// };
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
